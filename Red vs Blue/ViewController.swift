64c6d49283f5a680ee21837d6b3a3a701b928f51
Ashleigh Speer <ashleighspeer@gmail.com>
2015-12-16 13:14:44 +1100

    Initial Commit

---
+++ Red vs Blue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Red vs Blue
+//
+//  Created by Ashleigh Speer on 16/12/2015.
+//  Copyright © 2015 Ashleigh Speer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Red vs BlueTests/Red_vs_BlueTests.swift
@@ -0,0 +1,36 @@
+//
+//  Red_vs_BlueTests.swift
+//  Red vs BlueTests
+//
+//  Created by Ashleigh Speer on 16/12/2015.
+//  Copyright © 2015 Ashleigh Speer. All rights reserved.
+//
+
+import XCTest
+@testable import Red_vs_Blue
+
+class Red_vs_BlueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
